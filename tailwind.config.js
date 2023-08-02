42b4b0e57c8c3fed1d12aa2f7843b45983f6d5a3
davidmedev222 <davidmedev@gmail.com>
2023-08-02 14:34:45 -0300

    chore(tailwind/config): add color pallette for the brown color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,32 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        'fluid-3xl': 'clamp(2rem, 4vw, 4rem)'
+      },
+      colors: {
+        brown: {
+          50: '#f7f3ef',
+          100: '#ebe1d6',
+          200: '#d8c5b0',
+          300: '#c1a183',
+          400: '#ac7f5c',
+          500: '#9f7253',
+          600: '#895a45',
+          700: '#6e463a',
+          800: '#5e3c35',
+          900: '#523531',
+          950: '#2e1c1a'
+        }
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
       animation: {
         'mask-left': 'mask-left 1000ms linear forwards',
-        marquee: 'marquee 17000ms linear infinite',
-        marquee2: 'marquee2 17000ms linear infinite'
+        marquee: 'marquee 25000ms linear infinite',
+        marquee2: 'marquee2 25000ms linear infinite'
       },
       keyframes: {
         'mask-left': {
