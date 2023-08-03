ca84977ddbaab74c944106bc81a6be2ede74c600
davidmedev222 <davidmedev@gmail.com>
2023-08-02 22:13:51 -0300

    feat(ui): add UI of the call to action sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,36 @@
-import { BannerSection, WelcomeSection } from '@/components'
+import { BannerSection, CallToAction, WelcomeSection } from '@/components'
+import { cta2PhotoURL, cta3PhotoURL, ctaPhotoURL } from '@/constants'
 
 function HomePage() {
   return (
     <main>
       <BannerSection />
       <WelcomeSection />
+      <CallToAction
+        direction='ltr'
+        color='black'
+        photoURL={ctaPhotoURL}
+        title='Family Law'
+        paragraph='We understand that dealing with family law issues can be stressful and overwhelming, which
+          is why we strive to provide compassionate and personalized legal representation to each of
+          our clients. Whether you are going through a divorce, seeking child custody, or
+          considering adoption, our attorneys will work with you every step of the way to achieve
+          the best possible outcome for you and your family.'
+      />
+      <CallToAction
+        direction='rtl'
+        color='white'
+        photoURL={cta2PhotoURL}
+        title='Employment Law'
+        paragraph='At Buchannan & Morgan, our experienced employment law attorneys can help you with a variety of legal issues, including: Discrimination and harassment. Wrongful terminationWage and hour disputes.Employment contracts'
+      />
+      <CallToAction
+        direction='ltr'
+        color='black'
+        photoURL={cta3PhotoURL}
+        title='Personal Injury Law'
+        paragraph='Personal injury law is the area of law that deals with injuries caused by the negligence or wrongdoing of others. Our personal injury attorneys at Buchannan & Morgan are dedicated to helping clients who have been injured in accidents, including car accidents, slip and falls, and medical malpractice.'
+      />
     </main>
   )
 }
---
+++ src/components/cta/CallToAction.tsx
@@ -0,0 +1,59 @@
+import { brownBlurDataURL } from '@/constants'
+import clsx from 'clsx'
+import Image from 'next/image'
+import Button from '../button/Button'
+
+interface Props {
+  direction: 'ltr' | 'rtl'
+  title: string
+  paragraph: string
+  color: 'white' | 'black'
+  photoURL: string
+}
+
+function CallToAction({ direction, title, paragraph, color, photoURL }: Props) {
+  const classes = {
+    section: clsx(
+      'relative my-32 p-8 max-lg:space-y-14 lg:flex lg:items-center lg:gap-10',
+      color === 'black' && 'bg-gray-700 text-white',
+      color === 'white' && 'bg-gray-200 text-black'
+    ),
+    info: clsx(
+      'basis-2/5 space-y-4',
+      direction === 'ltr' && 'order-1',
+      direction === 'rtl' && 'order-2'
+    ),
+    picture: clsx(
+      'relative aspect-video basis-3/5 lg:translate-y-1/4',
+      direction === 'ltr' && 'order-2',
+      direction === 'rtl' && 'order-1'
+    )
+  }
+
+  return (
+    <section className={classes.section}>
+      <div className={classes.info}>
+        <h3 className='text-4xl font-extrabold'>{title}</h3>
+        <p className='text-base'>{paragraph}</p>
+        <Button
+          className='absolute bottom-5 left-1/2 z-10 -translate-x-1/2 lg:static lg:translate-x-0'
+          sizes='sm'
+        >
+          Contact us Today
+        </Button>
+      </div>
+      <div className={classes.picture}>
+        <Image
+          className='object-cover object-center'
+          src={photoURL}
+          alt='reunion de abogado con cliente'
+          blurDataURL={brownBlurDataURL}
+          placeholder='blur'
+          fill
+        />
+      </div>
+    </section>
+  )
+}
+
+export default CallToAction
